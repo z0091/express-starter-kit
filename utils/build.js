82a4a9b0acdc5ccb4aa954244ea3cbc05d0c90a6
Alexandr Plotnikov <p.a.ig.rus@gmail.com>
2017-07-18 19:49:59 +0400

    Fixed build.js

--- utils/build.js
+++ utils/build.js
@@ -1,7 +1,6 @@
 import run from './run';
 import clean from './clean';
 import bundle from './bundle';
-import bundleServer from './bundle.server';
 
 /**
  * Compiles the project from source files into a distributable
@@ -10,7 +9,6 @@ import bundleServer from './bundle.server';
 async function build() {
     await run(clean);
     await run(bundle);
-    await run(bundleServer);
 }
 
 module.exports = build;
